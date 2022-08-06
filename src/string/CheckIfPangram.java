87a16cabe5b1971a5cf29f5d57c5d3e79d488747
Hassan Refaat <76957887+hassanrefaat9@users.noreply.github.com>
2022-08-06 14:11:58 +0200

    new problems

--- src/string/CheckIfPangram.java
+++ src/string/CheckIfPangram.java
@@ -5,22 +5,32 @@ import java.util.HashSet;
 
 public class CheckIfPangram {
     public static void main(String[] args) {
-        String  sentence = "thequickbrownfoxjumpsoverthelazydog";
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
         System.out.println(checkIfPangram(sentence));
     }
 
     public static boolean checkIfPangram(String sentence) {
-        boolean [] str = new boolean[26];
-
+        boolean[] mark = new boolean[26];
+        boolean flag = false;
+        int index = 0;
         for (int i = 0; i < sentence.length(); i++) {
-            if(sentence.charAt(i) >=65 &&sentence.charAt(i) <=122){
-                str[i] = true;
+            char current = sentence.charAt(i);
+            if (current >= 'A' && current <= 'Z') {
+                index = current - 'A';
+            } else if (current >= 'a' && current <= 'z') {
+                index = current - 'a';
             }
+            mark[index] = true;
         }
-        for (int i = 0; i < str.length; i++) {
-            if(str[i] == false)
-                return false;
-        }
-        return true;
+
+        for (boolean i : mark)
+            if(i)
+                flag = true ;
+        else{
+            flag =false;
+            break;
+            }
+
+        return flag;
     }
 }
